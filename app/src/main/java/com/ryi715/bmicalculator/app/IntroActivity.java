4dc3ce45a8d7dea7477d8f135b53e22e802f1690
RYI <remyoucef@gmail.com>
2018-08-07 18:33:32 +0100

    create the base app classes and resources

---
+++ app/src/main/java/com/ryi715/bmicalculator/app/IntroActivity.java
@@ -0,0 +1,57 @@
+package com.ryi715.bmicalculator.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+import com.github.paolorotolo.appintro.AppIntro2;
+
+
+public class IntroActivity extends AppIntro2 {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getSupportActionBar().hide();
+
+        setGoBackLock(true);
+        showSkipButton(false);
+
+        addSlide(OneFragment.newInstance(R.layout.one_fragment));
+        addSlide(new TwoFragment());
+        addSlide(new ThreeFragment());
+        addSlide(new FourFragment());
+        addSlide(new FinalFragment());
+
+    }
+
+    @Override
+    public void onSkipPressed(Fragment currentFragment) {
+        super.onSkipPressed(currentFragment);
+        finish();
+    }
+
+    @Override
+    public void onDonePressed(Fragment currentFragment) {
+        super.onDonePressed(currentFragment);
+        finish();
+    }
+
+    @Override
+    public void onSlideChanged(@Nullable Fragment oldFragment, @Nullable Fragment newFragment) {
+        super.onSlideChanged(oldFragment, newFragment);
+
+        //do the rest
+    }
+
+
+   
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+    }
+}
